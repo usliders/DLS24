af199d6cd7ac3ad13cd3ad47e902ff0f77212678
Юрий Васильев <82704195+usliders@users.noreply.github.com>
2025-02-08 12:03:15 +0300

    n2

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ PROMTS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".", "data"
 LOG_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "bot_log.log"))
 encoded_image = None
 IMAGE_SIZE = (512, 512)
-API_HTTPS = "https://cadfedaa772af86ced.gradio.live"
+API_HTTPS = "https://6702bbc217bef80f74.gradio.live"
 TIME_ASK = 150
 GRADIO_STATIC_PATH = "D:/FooocusControl/Fooocus/outputs/gradio"
 
